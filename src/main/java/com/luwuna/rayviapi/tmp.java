24412b2a7abd8e3c3531d9fb424b48a093c02de6
Wolvinny <treblecorgi@gmail.com>
2022-05-28 18:28:10 +0200

    removed .class files (hopefully)

--- src/main/java/com/luwuna/rayviapi/tmp.java
+++ src/main/java/com/luwuna/rayviapi/tmp.java
@@ -1,9 +1,12 @@
 package com.luwuna.rayviapi;
 
 
+import static com.luwuna.rayviapi.Types.EntryType.USER;
+
 public class tmp {
     public static void main(String[] args){
        RavyAPI api = new RavyAPI("NDU1Nzc3NjAzNzA2NDIxMjQ5.03d2e6392cf27152ce0d021adcc43c389a1eb03e70059d56ecbe8df745892dc0");
        System.out.println(api.getScopes());
+       api.getBans("", USER);
     }
 }
