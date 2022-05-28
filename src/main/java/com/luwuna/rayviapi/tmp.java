1a7b0a853ca9a168ac70daf388d9de0354f10f8b
Wolvinny <treblecorgi@gmail.com>
2022-05-28 17:24:06 +0200

    Initial commit

---
+++ src/main/java/com/luwuna/rayviapi/tmp.java
@@ -0,0 +1,9 @@
+package com.luwuna.rayviapi;
+
+
+public class tmp {
+    public static void main(String[] args){
+       RavyAPI api = new RavyAPI("NDU1Nzc3NjAzNzA2NDIxMjQ5.03d2e6392cf27152ce0d021adcc43c389a1eb03e70059d56ecbe8df745892dc0");
+       System.out.println(api.getScopes());
+    }
+}
